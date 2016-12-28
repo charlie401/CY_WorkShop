74003ede65e6d0bcda179d40248f2f304fcdb781
charlie401 <charlie404@163.com>
2016-12-28 09:39:03 +0800

    new project
    new project

--- MyPlayground1.playground/Contents.swift
+++ MyPlayground1.playground/Contents.swift
@@ -9,14 +9,15 @@ var str = "Hello, playground"
 //let num = 1
 //let temp = num << 30//~num &+ 100
 //debugPrint(temp)
-func resolveValueWithDivisor(divisor:Double,dividend:Double = 10000) -> (remainder:Double,mulriple:Int64) {
-    var remainder = divisor
-    var times = 0
-    while remainder > dividend {
-        remainder /= dividend
-        times += 1
-    }
-    let mulriple = Int64(pow(Double(dividend), Double(times)))
-    return (remainder,mulriple)
-}
-resolveValueWithDivisor(1234)
+//func resolveValueWithDivisor(divisor:Double,dividend:Double = 10000) -> (remainder:Double,mulriple:Int64) {
+//    var remainder = divisor
+//    var times = 0
+//    while remainder > dividend {
+//        remainder /= dividend
+//        times += 1
+//    }
+//    let mulriple = Int64(pow(Double(dividend), Double(times)))
+//    return (remainder,mulriple)
+//}
+//resolveValueWithDivisor(1234)
+Double("77889.3hdjs")
